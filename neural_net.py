29c35e7927c8b8bf4b2b57bf5084d79071378242
Mathew <m4_wright@hotmail.com>
2018-03-06 17:26:52 +0000

    neural net added

--- neural_net.py
+++ neural_net.py
@@ -1,27 +1,32 @@
 import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
 
 
-BATCH_SIZE = 64
+BATCH_SIZE = 250
 INPUT_DIMENSION = 64*64
-HIDDEN_DIMENSION = 100
+HIDDEN_DIMENSION_1 = 100
+HIDDEN_DIMENSION_2 = 10
 OUT_DIMENSION = 1
 
 
-x = np.random.randn(BATCH_SIZE, INPUT_DIMENSION)
-y = np.random.randn(BATCH_SIZE, OUT_DIMENSION)
-
-w1 = np.random.randn(INPUT_DIMENSION, HIDDEN_DIMENSION)
-w2 = np.random.randn(HIDDEN_DIMENSION, OUT_DIMENSION)
+x = pd.read_csv("./data/head_x.csv", header=None).as_matrix()
+y = pd.read_csv("./data/head_y.csv", header=None).as_matrix()
 
 
-step_size = 1e-8
-steps = 10000
+w1 = np.random.randn(INPUT_DIMENSION, HIDDEN_DIMENSION_1)
+w2 = np.random.randn(HIDDEN_DIMENSION_1, HIDDEN_DIMENSION_2)
+w3 = np.random.randn(HIDDEN_DIMENSION_2, OUT_DIMENSION)
+
+
+step_size = 1e-7
+steps = 1000
 
 for step in range(steps):
     # Forward pass: compute predicted y
-    hidden_layer = x.dot(w1)
-    # print(hidden_layer)
-    hidden_relu = np.maximum(hidden_layer, 0)
+    hidden_layer_1 = x.dot(w1)
+    hidden_relu_1 = np.maximum(hidden_layer_1, 0)
+    
     predicted_y = hidden_relu.dot(w2)
 
     # Compute and print loss
@@ -43,3 +48,7 @@ for step in range(steps):
 
 
 print("error =", error)
+
+
+score = accuracy_score(y, predicted_y)
+print("score = ", score)
